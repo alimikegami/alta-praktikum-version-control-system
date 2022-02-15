68aff144530f880dd1542a52ce922b1be57015ca
Alim Ikegami <alimikegami1@gmail.com>
2022-02-15 16:17:29 +0800

    add comments to the code

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import "fmt"
 
+// function to greet the user
 func greet() {
 	fmt.Println("Hi, my name is Alim Ikegami! Nice to meet you!")
 }
 
+// function to ask question to the user
 func askQuestion() {
 	fmt.Println("How are you today?")
 }
 
+// the main function
 func main() {
 	fmt.Println("Hello, World!")
 	greet()
